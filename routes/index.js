52a36a2d4283e698bd02099ff7d860ea7c5f7cc0
KingArtisan <jonathanmatthewk@gmail.com>
2018-10-29 23:19:52 +0000

    Completed lab 4

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
+var fs = require('fs');
+var request = require('request');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile('weather.html', {root: 'public'});
+});
+
+/* GET city handler */
+router.get('/getcity',function(req,res,next) {
+    fs.readFile(__dirname + '/cities.dat.txt',function(err,data) {
+        if(err) throw err;
+        
+        var cities = data.toString().split("\n");
+        var jsonresult = [];
+        var myRe = new RegExp("^" + req.query.q);
+        //console.log(myRe);
+        
+        for(var i = 0; i < cities.length; i++) {
+          var result = cities[i].search(myRe); 
+          if(result != -1) {
+            //console.log(cities[i]);
+            jsonresult.push({city:cities[i]});
+          } 
+        }   
+        //console.log(jsonresult);
+        res.status(200).json(jsonresult);
+  });
+});
+
+/* GET owlroute handler */
+router.get('/owlroute',function(req,res,next) {
+  var api_url = "https://owlbot.info/api/v1/dictionary/"+req.query.q;
+  console.log(api_url);
+  request(api_url).pipe(res);
 });
 
 module.exports = router;
