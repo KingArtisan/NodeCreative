19d7b065ddde0d4532b0ec0b4bf30525d94035be
KingArtisan <jonathanmatthewk@gmail.com>
2018-11-02 00:18:54 +0000

    finished creative project

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,55 @@ router.get('/', function(req, res, next) {
   res.sendFile('weather.html', {root: 'public'});
 });
 
+/* GET name handler */
+router.get('/getname',function(req,res,next) {
+  //console.log(req.query);
+  fs.readFile(__dirname + '/'+req.query.race+'.names.dat.txt',function(err,data) {
+        if(err) throw err;
+        
+        var names = data.toString().toLowerCase().split("\n");
+        names.sort();
+        var jsonresult = [];
+        var myRe = new RegExp("^" + req.query.q.toLowerCase());
+        //console.log(myRe);
+        
+        for(var i = 0; i < names.length; i++) {
+          var result = names[i].search(myRe); 
+          if(result != -1) {
+            //console.log(names[i]);
+            jsonresult.push({name:names[i]});
+          } 
+        }   
+        //console.log(jsonresult);
+        res.status(200).json(jsonresult);
+  });
+});
+
+router.get('/getsurname',function(req,res,next) {
+  //console.log(req.query);
+  fs.readFile(__dirname + '/'+req.query.race+'.surnames.dat.txt',function(err,data) {
+        if(err) throw err;
+        
+        var names = data.toString().toLowerCase().split("\n");
+        names.sort();
+        var jsonresult = [];
+        var myRe = new RegExp("^" + req.query.q.toLowerCase());
+        //console.log(myRe);
+        
+        for(var i = 0; i < names.length; i++) {
+          var result = names[i].search(myRe); 
+          if(result != -1) {
+            //console.log(names[i]);
+            jsonresult.push({surname:names[i]});
+          } 
+        }   
+        //console.log(jsonresult);
+        res.status(200).json(jsonresult);
+  });
+});
+
 /* GET city handler */
-router.get('/getcity',function(req,res,next) {
+/*router.get('/getcity',function(req,res,next) {
     fs.readFile(__dirname + '/cities.dat.txt',function(err,data) {
         if(err) throw err;
         
@@ -28,13 +75,13 @@ router.get('/getcity',function(req,res,next) {
         //console.log(jsonresult);
         res.status(200).json(jsonresult);
   });
-});
+});*/
 
 /* GET owlroute handler */
-router.get('/owlroute',function(req,res,next) {
+/*router.get('/owlroute',function(req,res,next) {
   var api_url = "https://owlbot.info/api/v1/dictionary/"+req.query.q;
   console.log(api_url);
   request(api_url).pipe(res);
-});
+});*/
 
 module.exports = router;
